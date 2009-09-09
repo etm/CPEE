2b780ba1998aa1fdb683ef8f5cea880a5ba2b3e6
Gerhard Stürmer <roemer@roemer-laptop.(none)>
2009-09-09 15:02:54 +0200

    added description stuff

---
+++ examples/dist-wee/lib/description.rb
@@ -0,0 +1,21 @@
+require ::File.dirname(__FILE__) + '/MarkUS_V3.0'
+
+class DescriptionGET < Riddl::Implementation
+  def response
+    pp "DescriptionGET, r0=#{@r[0]}"
+    instance_id = @r[0].to_i
+    wf = $controller[instance_id]
+    pp "Description: #{wf.wf_description.inspect}"
+    Riddl::Parameter::Simple.new("description",wf.wf_description)
+  end
+end
+
+class DescriptionPUT < Riddl::Implementation
+  def response
+    pp "DescriptionPUT, r0=#{@r[0]}, p0=#{@p[0].value}"
+    instance_id = @r[0].to_i
+    description = @p[0].value
+    wf = $controller[instance_id]
+    wf.wf_description= description
+  end
+end
