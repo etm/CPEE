67f16a8319defac1c2da3d4e54200e59ac9b348c
Gerhard Stürmer <roemer@roemer-laptop.(none)>
2009-09-17 13:14:39 +0200

    Problems with MonitorGET to fix

---
+++ examples/dist-wee/lib/MonitoringHandler.rb
@@ -0,0 +1,82 @@
+# require 'Thread'
+require ::File.dirname(__FILE__) + '/../../../lib/Wee'
+require ::File.dirname(__FILE__) + '/../../../../riddl/lib/ruby/client'
+
+class MonitoringHandler < Wee::HandlerWrapperBase
+  def initialize(url)
+    p "MonitoringHandler.initialize: url = #{url.inspect}"
+    url = url.is_a?(String) ? url : url[0];
+    srv = Riddl::Client.new(/(.*:\/\/.*:[0-9]*\/)(.*)/.match(url)[1])
+    @logresource = srv.resource(/(.*:\/\/.*:[0-9]*\/)(.*)/.match(url)[2])
+
+    @__myhandler_stopped = false
+    @__myhandler_finished = false
+    @__myhandler_returnValue = nil
+  end
+
+  def log(type, details)
+    p "[#{Time.now.to_s}] #{type}: #{details}"
+    @logresource.request :post => [
+      Riddl::Parameter::Simple.new("stamp", Time.now.to_s),
+      Riddl::Parameter::Simple.new("type", type),
+      Riddl::Parameter::Simple.new("details", details)
+    ]
+  end
+
+  # executes a ws-call to the given endpoint with the given parameters. the call
+  # can be executed asynchron, see finished_call & return_value
+  def handle_call(position, passthrough, endpoint, *parameters)
+    log "handle_call", "Handle call: passthrough=[#{passthrough}], endpoint=[#{endpoint}], parameters=[#{parameters}]"
+    Thread.new do
+      sleep(0.6)
+      return if @__myhandler_stopped
+      @__myhandler_finished = true
+      @__myhandler_returnValue = 'Handler_Dummy_Result'
+    end
+  end
+
+  # returns true if the last handled call has finished processing, or the
+  # call runs independent (asynchronous call)
+  def finished_call
+    return @__myhandler_finished
+  end
+
+  # returns the result of the last handled call
+  def return_value
+    @__myhandler_finished ? @__myhandler_returnValue : nil
+  end
+  # Called if the WS-Call should be interrupted. The decision how to deal
+  # with this situation is given to the handler. To provide the possibility
+  # of a continue the Handler will be asked for a passthrough
+  def stop_call
+    log "stop_call", "Recieved stop signal, aborting on next possibility"
+    @__myhandler_stopped = true
+  end
+  # is called from Wee after stop_call to ask for a passthrough-value that may give
+  # information about how to continue the call. This passthrough-value is given
+  # to handle_call if the workflow is configured to do so.
+  def passthrough
+    nil
+  end
+
+  # Called if the execution of the actual handle_call is not necessary anymore
+  # It is definit that the call will not be continued.
+  # At this stage, this is only the case if parallel branches are not needed
+  # anymore to continue the workflow
+  def no_longer_necessary
+    log "MyHandler.stop_call", "Recieved no_longer_necessary signal, aborting on next possibility"
+    @__myhandler_stopped = true
+  end
+  # Is called if a Activity is executed correctly
+  def inform_activity_done(activity, context)
+    log "MyHandler.inform_activity_done", "Activity #{activity} done"
+  end
+  # Is called if a Activity is executed with an error
+  def inform_activity_failed(activity, context, err)
+    log "inform_activity_failed", "Activity #{activity} failed with error #{err}"
+    raise(err)
+  end
+  def inform_workflow_state(newstate)
+    log "MyHandler.inform_workflow_state", "State changed to #{newstate}"
+  end
+end
---
+++ examples/ui/lib/monitor.rb
@@ -0,0 +1,28 @@
+require ::File.dirname(__FILE__) + '/MarkUS_V3.0'
+require 'pp'
+
+class MonitorGET < Riddl::Implementation
+  include MarkUSModule
+
+  def response
+    pp "MonitorGET"
+    $monitor_log ||= []
+
+    Riddl::Parameter::Simple.new("file","text/xml") do
+      log_ do
+        $monitor_log.each do |line|
+          entry_ :stamp => line[:stamp], :type => line[:type], :details => line[:details]
+        end
+      end
+    end
+  end
+end
+
+class MonitorPOST < Riddl::Implementation
+  def response
+    pp "MonitorPOST, p0=#{@p[0].value}, p1=#{@p[1].value}, p2=#{@p[2].value}"
+    $monitor_log ||= []
+    $monitor_log.push({:stamp => @p[0].value, :type => @p[1].value, :details => @p[2].value})
+    
+  end
+end
