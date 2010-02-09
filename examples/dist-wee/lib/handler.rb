fcaa3269ef39d26c50d0c28a1cd9c7fa4f98d263
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2010-02-09 18:24:25 +0100

    handler.rb dist-wee fix

--- examples/dist-wee/lib/handler.rb
+++ examples/dist-wee/lib/handler.rb
@@ -8,7 +8,7 @@ class HandlerPOST < Riddl::Implementation
     pp "HandlerPOST, r0=#{@r[0]}, p0=#{@p[0].value}, p1=#{@p[1].value}"
     instance_id = @r[0].to_i
     classname = @p[0].value
-    arg = @p[1].value
+    arg = [@p[1].value]
     wf = $controller[instance_id]
     wf.handler=Kernel.const_get(classname)
     p "HandlerPOST => #{arg}"
