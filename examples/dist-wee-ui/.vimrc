fba02dd80479d47e9a49182550563d3e03e8626e
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2010-04-16 15:20:37 +0200

    removal of dist-wee after defensio, there is a tag dist-wee for last wee version

--- examples/dist-wee-ui/.vimrc
+++
@@ -1,2 +0,0 @@
-"map <F10> :!rackup server.ru<CR>
-map <F10> :!~/gerhard.start<CR>
--- examples/dist-wee/lib/name.rb
+++
@@ -1,6 +0,0 @@
-class NameGET < Riddl::Implementation
-  def response
-    instance_id = @r[0].to_i
-    Riddl::Parameter::Simple.new("name",$controller.name[instance_id])
-  end
-end
