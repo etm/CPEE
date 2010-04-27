8f5d17186ea585cc2c5bcdb4be244c0db9b28140
Ralph Vigne <ralph.vigne@gmail.com>
2010-04-27 21:40:30 +0200

    rescue injection

---
+++ examples/cpee/clear_instances.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm instances/1* -r
+rm instances/2* -r
+rm instances/3* -r
+rm instances/4* -r
+rm instances/5* -r
+rm instances/6* -r
+rm instances/7* -r
+rm instances/8* -r
+rm instances/9* -r
