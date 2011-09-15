57554fdde47de3d5dd717ef218dd0bf2124e7f56
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2011-09-15 23:14:08 +0200

    debug and production server with commandline arguments

--- server/.vimrc
+++ server/.vimrc
@@ -1,2 +1,2 @@
-map <F10> :!reset;ruby server.rb<CR>
-map <F9> :!reset;rm -rf instances/?;rm -rf instances/??;rm -rf instances/???;ruby server.rb<CR>
+map <F10> :!reset;./server.rb -v start<CR>
+map <F9> :!reset;rm -rf instances/?;rm -rf instances/??;rm -rf instances/???;ruby server.rb -v start<CR>
--- server/server.rb
+++ server/server.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/ruby
 $port = 9298
-$url  = 'http://localhost:' + $port.to_s
+$host = 'http://localhost'
 $mode = :debug # :production
-$0    = "cpee"
 
 if File.exists?(File.expand_path(File.dirname(__FILE__) + '/server.config.rb'))
   require File.expand_path(File.dirname(__FILE__) + '/server.config')
 end  
+
+$0    = "CPEE"
+$url  = $host + ':' + $port.to_s
 
 require 'pp'
 require 'fileutils'
@@ -19,7 +21,7 @@ require 'riddl/utils/fileserve'
 require 'riddl/utils/declaration'
 require './engine/implementation'
 
-rsrv = Riddl::Server.new(::File.dirname(__FILE__) + '/server.declaration.xml') do
+rsrv = Riddl::Server.new(::File.dirname(__FILE__) + '/server.declaration.xml',File.expand_path(::File.dirname(__FILE__))) do
   accessible_description true
   cross_site_xhr true
 
@@ -30,7 +32,7 @@ rsrv = Riddl::Server.new(::File.dirname(__FILE__) + '/server.declaration.xml') d
     :overview => '/xsls/overview.xsl',
     :subscriptions => '/xsls/subscriptions.xsl'
   }
-  
+
   on resource do
     run Instances if get '*'
     run NewInstance, $url if post 'instance-name'
@@ -66,9 +68,85 @@ rsrv = Riddl::Server.new(::File.dirname(__FILE__) + '/server.declaration.xml') d
   end
 end
 
-Rack::Server.start(
-  :app => rsrv,
-  :Port => $port,
-  :environment => 'development',
-  :server => 'mongrel'
-)
+########################################################################################################################
+# parse arguments
+########################################################################################################################
+verbose = false
+operation = "start"
+ARGV.options { |opt|
+  opt.summary_indent = ' ' * 4
+  opt.banner = "Usage:\n#{opt.summary_indent}ruby server.rb [options] start|stop|restart|info\n"
+  opt.on("Options:")
+  opt.on("--verbose", "-v", "Do not daemonize. Write ouput to console.") { verbose = true }
+  opt.on("--help", "-h", "This text.") { puts opt; exit }
+  opt.separator(opt.summary_indent + "start|stop|restart|info".ljust(opt.summary_width+1) + "Do operation start, stop, restart or get information")
+  opt.parse!
+}
+unless %w{start stop restart info}.include?(ARGV[0])
+  puts ARGV.options
+  exit
+end
+operation = ARGV[0]
+
+########################################################################################################################
+# status and info
+########################################################################################################################
+pid = File.read('server.pid') rescue pid = 666
+status = `ps -u #{Process.uid} | grep "#{pid} "`.scan(/ server\.[^\s]+/)
+if operation == "info" && status.empty?
+  puts "Server (#{$url}) not running"
+  exit
+end
+if operation == "info" && !status.empty?
+  puts "Server (#{$url}) running as #{pid}"
+  stats = `ps -o "vsz,rss,stime,time" -p #{pid}`.split("\n")[1].strip.split(/ +/)
+  puts "Virtual:  #{"%0.2f" % (stats[0].to_f/1024)} MiB"
+  puts "Resident: #{"%0.2f" % (stats[1].to_f/1024)} MiB"
+  puts "Started:  #{stats[2]}"
+  puts "CPU Time: #{stats[3]}"
+  exit
+end
+if operation == "start" && !status.empty?
+  puts "Server (#{$url}) already started"
+  exit
+end
+
+########################################################################################################################
+# stop/restart server
+########################################################################################################################
+if %w{stop restart}.include?(operation)
+  if status.empty?
+    puts "Server (#{$url}) maybe not started?"
+  else
+    puts "Server (#{$url}) stopped"
+    `kill #{pid}`
+    puts "Waiting for 2 seconds to accomplish ..."
+    sleep 2 if operation == "restart"
+  end
+  exit unless operation == "restart"
+end
+
+########################################################################################################################
+# start server
+########################################################################################################################
+server = if verbose
+  Rack::Server.new(
+    :app => rsrv,
+    :Port => $port,
+    :environment => ($mode == :debug ? 'development' : 'deployment'),
+    :server => 'mongrel',
+    :pid => File.expand_path(File.dirname(__FILE__) + '/server.pid')
+  )
+else
+  server = Rack::Server.new(
+    :app => rsrv,
+    :Port => $port,
+    :environment => 'none',
+    :server => 'mongrel',
+    :pid => File.expand_path(File.dirname(__FILE__) + '/server.pid'),
+    :daemonize => true
+  )
+end
+
+puts "Server (#{$url}) started"
+server.start
