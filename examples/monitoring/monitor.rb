607913c8497d70175a7334f46e329b5470a7e7ce
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2012-03-16 00:32:16 +0100

    leaner monitoring example

--- examples/monitoring/monitor.rb
+++ examples/monitoring/monitor.rb
@@ -1,24 +1,20 @@
 #!/usr/bin/ruby
-require 'pp'
-require 'fileutils'
-
 require 'rubygems'
 gem 'riddl', '>=0.99.30'
 require 'riddl/client'
 
 srv = Riddl::Client.new("http://localhost:9298/")
-res = srv.resource("/")
 
 # new instance
-status, response = res.post [
+status, response = srv.resource("/").post [
   Riddl::Parameter::Simple.new("name","Monitor Test")
 ]
 ins = response.first.value
 
+# if instance not empty monitor it
 unless ins.empty?
   puts "Monitoring Instance #{ins}"
-  res = srv.resource("/#{ins}/notifications/subscriptions/")
-  status, response = res.post [ 
+  status, response = srv.resource("/#{ins}/notifications/subscriptions/").post [ 
     Riddl::Parameter::Simple.new("topic","properties/description"),
     Riddl::Parameter::Simple.new("events","change"),
     Riddl::Parameter::Simple.new("topic","properties/state"),
