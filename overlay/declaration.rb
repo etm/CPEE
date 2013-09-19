8e2e923b6dae93e22c3d9f87862fb632f856791a
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2013-09-19 22:53:30 +0200

    overlay fixed

--- overlay/declaration.rb
+++ overlay/declaration.rb
@@ -8,15 +8,15 @@ Riddl::Server.new(File.dirname(__FILE__) + '/declaration.xml', :port => 9297) do
   accessible_description false
 
   interface 'xsloverlay' do
-    run Riddl::Utils::XSLOverlay, "/xsls/instances.xsl"     if get && declaration_resource == '/'
-    run Riddl::Utils::XSLOverlay, "/xsls/info.xsl"          if get && declaration_resource == '/{}'
-    run Riddl::Utils::XSLOverlay, "/xsls/properties.xsl"    if get && declaration_resource == '/{}/properties'
-    run Riddl::Utils::XSLOverlay, "/xsls/notifications.xsl" if get && declaration_resource == '/{}/notifications'
-    run Riddl::Utils::XSLOverlay, "/xsls/subscriptions.xsl" if get && declaration_resource == '/{}/notifications/subscriptions'
-    run Riddl::Utils::XSLOverlay, "/xsls/values.xsl"        if get && declaration_resource == '/{}/properties/values'
-    run Riddl::Utils::XSLOverlay, "/xsls/subs.xsl"          if get && declaration_resource == '/{}/notifications/subscriptions/{}'
-    run Riddl::Utils::XSLOverlay, "/xsls/callback.xsl"      if get && declaration_resource == '/{}/callbacks'
-    run Riddl::Utils::XSLOverlay, "/xsls/topics.xsl"        if get && declaration_resource == '/{}/notifications/topics'
+    run Riddl::Utils::XSLOverlay, "/xsls/instances.xsl"     if get('xmlin') && declaration_resource == '/'
+    run Riddl::Utils::XSLOverlay, "/xsls/info.xsl"          if get('xmlin') && declaration_resource == '/{}'
+    run Riddl::Utils::XSLOverlay, "/xsls/properties.xsl"    if get('xmlin') && declaration_resource == '/{}/properties'
+    run Riddl::Utils::XSLOverlay, "/xsls/notifications.xsl" if get('xmlin') && declaration_resource == '/{}/notifications'
+    run Riddl::Utils::XSLOverlay, "/xsls/subscriptions.xsl" if get('xmlin') && declaration_resource == '/{}/notifications/subscriptions'
+    run Riddl::Utils::XSLOverlay, "/xsls/values.xsl"        if get('xmlin') && declaration_resource == '/{}/properties/values'
+    run Riddl::Utils::XSLOverlay, "/xsls/subs.xsl"          if get('xmlin') && declaration_resource == '/{}/notifications/subscriptions/{}'
+    run Riddl::Utils::XSLOverlay, "/xsls/callback.xsl"      if get('xmlin') && declaration_resource == '/{}/callbacks'
+    run Riddl::Utils::XSLOverlay, "/xsls/topics.xsl"        if get('xmlin') && declaration_resource == '/{}/notifications/topics'
   end
 
   interface 'xsls' do
