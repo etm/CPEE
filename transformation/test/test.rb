a310ff78fcc9d2e0fcab29a8c878a9b0027301b3
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2014-07-11 17:02:02 +0200

    test transformation example 11

--- transformation/test/test.rb
+++ transformation/test/test.rb
@@ -6,16 +6,8 @@ require File.expand_path(File.dirname(__FILE__) + '/../lib/cpee')
 require 'pp'
 
 Dir.chdir(File.expand_path(File.dirname(__FILE__)))
-f = "Test 1.bpmn"
+f = "Test 11.bpmn"
 
 bpmn2 = ProcessTransformation::Source::BPMN2.new(File.read(f))
-
-p bpmn2.dataelements
-p bpmn2.endpoints
-
-
 bpmn2.traces
-tree = bpmn2.tree(false).to_s
-xml = bpmn2.model(ProcessTransformation::Target::CPEE)
-
-puts tree
+tree = bpmn2.tree(true).to_s
