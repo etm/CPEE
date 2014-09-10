6f5c82ffb8a194fdb5fe0c5607375be1e8d4ad81
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2014-09-10 04:41:28 +0200

    signavio handlerwrapper fixes

--- lib/cpee/processtransformation/cpee.rb
+++ lib/cpee/processtransformation/cpee.rb
@@ -77,9 +77,9 @@ module CPEE
             n   = res.add('d:call', 'id' => "a#{node.niceid}", 'endpoint' => node.endpoints.join(','))
             p   = n.add('d:parameters')
                   p.add('d:label',node.label)
-                  p.add('d:method',node.methods.join(','))
-                  p.add('d:type',node.type)
-                  p.add('d:mid',node.id)
+                  p.add('d:method',node.methods.join(',') || 'post')
+                  p.add('d:type',":#{node.type}")
+                  p.add('d:mid',"'#{node.id}'")
             par = p.add('d:parameters')
             node.parameters.each do |k,v|
               par.add(k,v)
