6d783e148e31c2ed7017fe9a18ad0ee83e97349c
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2015-06-08 11:18:03 +0200

    flo testset for instantiation

--- instantiation/test.rb
+++ instantiation/test.rb
@@ -3,10 +3,10 @@ require 'rubygems'
 require 'riddl/server'
 require 'xml/smart'
 
-$inst = 'http://localhost:9296/'
+$inst = 'http://cpee.org:9296/'
 
 srv = Riddl::Client.new($inst, $inst + "?riddl-description")
-status, res = srv.post Riddl::Parameter::Complex.new('xml','text/xml',File.read(File.dirname(__FILE__) + '/testset.xml'))
+status, res = srv.post Riddl::Parameter::Complex.new('xml','text/xml',File.read(File.dirname(__FILE__) + '/flo.xml'))
 
 puts status
 p res
