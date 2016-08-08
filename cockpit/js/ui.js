b32061353064a08d7c357be4fdc29d96d7f8d8a8
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2016-08-09 00:19:12 +0200

    more cruft removed

--- cockpit/js/ui.js
+++ cockpit/js/ui.js
@@ -24,27 +24,3 @@ $(document).ready(function() {
     });
   }
 });
-
-// flexible parameter input
-function remove_entry(target,foc) { // {{{
-  var tr = $($(target).parents('tr').get(0));
-  if (foc) {
-    var par = tr.parent();
-    $('input.' + $(target).attr('class'),par).each(function(){
-      if (this == target) {
-        if (prev) prev.focus();
-        return false;
-      }
-      prev = this;
-    });
-  }
-  tr.remove();
-} // }}}
-
-function new_entry(top) { // {{{
-  var visid = $('ui-tabbar ui-tab',top).not('.switch').not('.inactive').attr('id').replace(/tab/,'');
-  var node = $('#dat_template_pair tr').clone();
-  var vnode = $('#dat_' + visid).append(node);
-  $('.pair_name',vnode).focus();
-  return node;
-} // }}}
