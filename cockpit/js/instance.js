7ba999a3df287af07f24b0244d65a67694874d52
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2018-01-24 08:06:17 +0100

    instance fix

--- cockpit/js/instance.js
+++ cockpit/js/instance.js
@@ -1,4 +1,5 @@
 var ws;
+var myid = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
 var running = false;
 var subscription;
 var subscription_state = 'less';
