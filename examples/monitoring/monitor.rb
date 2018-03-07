4f3be2c584437d0189f4b4e592c7bc1f34fb1d2a
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2018-03-07 23:35:44 +0100

    monitor

--- examples/monitoring/monitor.rb
+++ examples/monitoring/monitor.rb
@@ -3,29 +3,36 @@ require 'rubygems'
 gem 'riddl', '>=0.99.30'
 require 'riddl/client'
 
-srv = Riddl::Client.new("http://localhost:9298/")
+srv = Riddl::Client.new("https://centurio.work:9298/")
 
 # new instance
-status, response = srv.resource("/").post [
-  Riddl::Parameter::Simple.new("name","Monitor Test")
-]
-ins = response.first.value
+# status, response = srv.resource("/").post [
+#   Riddl::Parameter::Simple.new("info","Monitor Test")
+# ]
+# ins = response.first.value
+
+ins = "371"
 
 # if instance not empty monitor it
 unless ins.empty?
   puts "Monitoring Instance #{ins}"
-  status, response = srv.resource("/#{ins}/notifications/subscriptions/").post [ 
-    Riddl::Parameter::Simple.new("topic","properties/description"),
+  status, response = srv.resource("/#{ins}/notifications/subscriptions/").post [
+    Riddl::Parameter::Simple.new("topic","description"),
     Riddl::Parameter::Simple.new("events","change"),
-    Riddl::Parameter::Simple.new("topic","properties/state"),
+    Riddl::Parameter::Simple.new("topic","state"),
     Riddl::Parameter::Simple.new("events","change"),
   ]
   key = response.first.value
 
   res = srv.resource("/#{ins}/notifications/subscriptions/#{key}/ws/").ws do |conn|
-    conn.stream do |msg|
-      puts msg
+    conn.on :close do
+      EM::stop_event_loop
+    end
+
+    conn.on :message do |msg|
+      p msg.data
+      conn.close
       puts '--------------'
     end
-  end  
+  end
 end
