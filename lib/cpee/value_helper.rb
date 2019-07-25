7f3479912ca936c030612c075743fd02f0b2b82e
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2019-07-25 19:21:21 +0200

    extenden parsing for opcua

--- lib/cpee/value_helper.rb
+++ lib/cpee/value_helper.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'time'
+
 module CPEE
 
   class ValueHelper
@@ -27,6 +30,19 @@ module CPEE
           end
       end
     end
+
+    def self::parse_extended(value)
+      if [String].include? value.class
+        self::parse(value)
+      elsif [Array, NilClass, Integer, Float, TrueClass, FalseClass, Date].inclu
+        value
+      elsif value.respond_to?(:to_s)
+        value.to_s
+      else
+        ''
+      end
+    end
+
   end
 
 end
