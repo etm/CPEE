c4eba56837a2be209750022132e12e4a71930a41
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2019-11-26 20:26:32 +0100

    azure logger

---
+++ log/azure.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+require_relative 'azure_logger'
+
+Riddl::Server.new(File.join(__dir__,'/log.xml'), :host => 'localhost', :port => 9313) do
+  accessible_description true
+  cross_site_xhr true
+  interface 'events' do
+	  run Logging if post 'event'
+  end
+end.loop!
