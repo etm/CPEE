b52d7666050fb00d174454346821366b5ef7bae8
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2020-10-02 00:06:06 +0200

    moved log out

--- test/callback.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby
-require 'riddl/client'
-
-client = Riddl::Client.new("http://localhost:9298/39/callbacks/#{ARGV[1]}")
-status, response, headers = client.put [
-  Riddl::Header.new('CPEE-UPDATE','true'),
-  Riddl::Header.new('CPEE-UPDATE-STATUS','georg'),
-  Riddl::Parameter::Simple.new('x',ARGV[0])
-]
