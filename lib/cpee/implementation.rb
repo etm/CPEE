5add264d6208e5e4fc46a3c699fc52513e2df53d
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2020-10-05 13:54:33 +0200

    fixed error in subservice start

--- lib/cpee/implementation.rb
+++ lib/cpee/implementation.rb
@@ -90,7 +90,7 @@ module CPEE
       parallel do
         CPEE::watch_services(@riddl_opts[:watchdog_start_off])
         EM.add_periodic_timer(@riddl_opts[:watchdog_frequency]) do
-          CPEE::watch_services
+          CPEE::watch_services(@riddl_opts[:watchdog_start_off])
         end
       end
       cleanup do
@@ -125,15 +125,15 @@ module CPEE
 
   def self::watch_services(watchdog_start_off)
     return if watchdog_start_off
-    EM.defer do
-      Dir[File.join(__dir__,'..','..','server','routing','*.rb')].each do |s|
-        s = s.sub(/\.rb$/,'')
-        pid = (File.read(s + '.pid').to_i rescue nil)
-        if (pid.nil? || !(Process.kill(0, pid) rescue false)) && !File.exist?(s + '.lock')
-          system "#{s}.rb restart 1>/dev/null 2>&1"
-          puts "➡ Service #{File.basename(s,'.rb')} started ..."
-        end
-      end
+     EM.defer do
+       Dir[File.join(__dir__,'..','..','server','routing','*.rb')].each do |s|
+         s = s.sub(/\.rb$/,'')
+         pid = (File.read(s + '.pid').to_i rescue nil)
+         if (pid.nil? || !(Process.kill(0, pid) rescue false)) && !File.exist?(s + '.lock')
+           system "#{s}.rb restart 1>/dev/null 2>&1"
+           puts "➡ Service #{File.basename(s,'.rb')} started ..."
+         end
+       end
     end
   end
   def self::cleanup_services(watchdog_start_off)
