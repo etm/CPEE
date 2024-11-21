242c0c440ac9705822afdc9ae61a53c9f1239fd2
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2024-11-21 22:30:45 +0100

    graph_highlight fix and scroll into view

--- cockpit/js/instance.js
+++ cockpit/js/instance.js
@@ -132,7 +132,7 @@ function cockpit() { //{{{
       if (q.theme) { graph_theme = q.theme; }
       if (q.position) { graph_position = q.position == 'false' ? 'false' : 'true'; }
       if (q.highlight) {
-        graph_highlight = q.highlight.match(/((a\d+,)+)([a-z0-9]{6})/);
+        graph_highlight = q.highlight.match(/((a\d+,)+)([a-z0-9]{6,8})$/);
         if (graph_highlight.length > 0) {
           graph_highlight_color = graph_highlight[3];
           graph_highlight_tasks = graph_highlight[1].split(',');
@@ -580,9 +580,12 @@ function adaptor_init(url,theme,dslx) { //{{{
         // highlight
 
         if (graph_highlight) {
-          let er = $('g[element-id=a1]').attr('element-row');
-          styletext = 'svg rect[element-row="'+ er + '"], div.graphlast[element-row="'+ er + '"] {  fill: #' + graph_highlight_color + ' !important; background-color:  #' + graph_highlight_color + ' !important; } ';
-          $('head').append('<style type="text/css">' + styletext + '</style>');
+          graph_highlight_tasks.forEach((ele) => {
+            let er = $('g[element-id=' + ele + ']').attr('element-row');
+            styletext = 'svg rect[element-row="'+ er + '"], div.graphlast[element-row="'+ er + '"] {  fill: #' + graph_highlight_color + ' !important; background-color:  #' + graph_highlight_color + ' !important; } ';
+            $('head').append('<style type="text/css">' + styletext + '</style>');
+            scroll_into_view(ele);
+          })
         }
 
         // labels
