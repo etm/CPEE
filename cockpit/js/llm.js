fb6164415f455af31abdb5dcbeb318276a7aa05f
Nataliia 'com-pot' Klievtsova <n.klievtsova@gmail.com>
2025-06-25 22:53:49 +0200

    add new view for the cpee-llm extention

--- cockpit/js/llm.js
+++ cockpit/js/llm.js
@@ -1,3 +1,6 @@
+var last_generated_model = undefined;
+var last_model_before_generation =undefined;
+
 function clean_llm_ui(status_id) {
   status_div = $(`#${status_id}`);
   status_div.empty();
@@ -5,16 +8,32 @@ function clean_llm_ui(status_id) {
   return
 }
 
-function call_llm_service(status_id,prompt_id) {
+function add_prompt(prompt_id,content) {
+  let input = $(`#${prompt_id}`);
+  const range = document.createRange();
+  const selection = window.getSelection();
+  range.selectNodeContents(input[0]);
+  selection.removeAllRanges();
+  selection.addRange(range);
+  document.execCommand('insertText', false, content);
+  return ;
+}
+
+function call_llm_service(status_id,prompt_id,llm_id) {
   let input = $(`#${prompt_id}`);
   let text = input[0].innerText;
-
+  let myllm = $(`#${llm_id}`).find(":selected").val();
+  console.log(myllm);
+  if (myllm === undefined){
+    myllm = "gemini-2.0-flash";
+  }
+  console.log(myllm);
   const formData = new FormData();
   const blob1 = new Blob([save['dslx']], { type: "text/xml" });
   formData.append("rpst_xml", blob1);
   const blob2 = new Blob([text], { type: "text/plain" });
   formData.append("user_input", blob2);
-  const blob3 = new Blob(['gemini-2.0-flash'], { type: "text/plain" });
+  const blob3 = new Blob([myllm], { type: "text/plain" });
   formData.append("llm", blob3);
 
   jQuery.ajax({
@@ -25,34 +44,139 @@ function call_llm_service(status_id,prompt_id) {
     processData: false,
     method: 'POST',
     success: function(data){
-      $.ajax({
-        type: "PUT",
-        url: url + "/properties/description/",
-        contentType: 'text/xml',
-        headers: { 'Content-ID': 'description' },
-        data: data.output_cpee
-      });
-      $(`#${status_id}`).text(data.status);
-      $(`#${status_id}`).addClass('success');
+      last_model_before_generation = save['dslx'];
+      console.log(data);
+      last_generated_model = data.output_cpee;
+      set_cpee_model(data.output_cpee);
+      set_success(status_id,data.status);
     },
     error:  function(xhr, status, data) {
-      $(`#${status_id}`).text(xhr.responseJSON.error);
-      $(`#${status_id}`).addClass('error');
+      set_error(status_id,xhr.responseJSON.error);
     }
   });
 
  input.empty();
 }
 
+function call_llm_text_service(status_id,prompt_id,llm_id,action) {
+  let myllm = $(`#${llm_id}`).find(":selected").val();
+  if (myllm === undefined){
+    myllm = "gemini-2.0-flash";
+  }
+  const info = save.attributes_raw.info;
+  const formData = new FormData();
+  const first = new Blob([save['dslx']], { type: "text/xml" });
+  formData.append("rpst_xml", first);
+  const second = myllm;
+  console.log(second);
+  formData.append("llm", second);
+
+  jQuery.ajax({
+    url: '/llm/text/llm/',
+    data: formData,
+    cache: false,
+    contentType: false,
+    processData: false,
+    method: 'POST',
+    success: function(data){
+      console.log(data["output_text"]);
+      if (action=="show"){
+        add_prompt(prompt_id,data["output_text"]);
+      } else if (action=="file") {
+        $('#savetext').attr('download', info + '.txt');
+        const encodedText = encodeURIComponent(data["output_text"]);
+        const dataUri = "data:text/plain;charset=utf-8," + encodedText;
+        const link = document.getElementById("savetext");
+        link.href = dataUri;
+        link.click();
+      };
+      last_model_before_generation = save['dslx'];
+      set_success(status_id,data.status);
+    },
+    error:  function(xhr, status, data) {
+      set_error(status_id,xhr.responseJSON.error);
+    }
+  });
+}
+
+function set_cpee_model(cpee_xml) {
+  $.ajax({
+    type: "PUT",
+    url: url + "/properties/dslx/",
+    contentType: 'text/xml',
+    headers: { 'Content-ID': 'dslx' },
+    data: cpee_xml
+  });
+}
+
+function set_success(status_id,success_text) {
+  $(`#${status_id}`).text(success_text);
+  $(`#${status_id}`).addClass('success');
+}
+
+function set_error(status_id,error_text) {
+   $(`#${status_id}`).text(error_text);
+   $(`#${status_id}`).addClass('error');
+}
+
+function load_last_generated_model() {
+  set_cpee_model(last_generated_model === undefined ? save['dslx'] : last_generated_model);
+}
+
+function load_last_model_before_generation() {
+  set_cpee_model(last_model_before_generation === undefined ? save['dslx'] : last_model_before_generation);
+}
+
+function load_file_content(files) {
+  if (typeof window.FileReader !== 'function') {
+    alert('FileReader not yet supportet');
+    return;
+  }
+  var reader = new FileReader();
+  reader.onload = function(){
+    clean_llm_ui('status');
+    add_prompt('prompt',reader.result);
+  }
+  reader.onerror = function(){ console.log("reader error"); }
+  reader.onabort = function(){ console.log("reader abort"); }
+  reader.readAsText(files[0]);
+}
+
 $(document).ready(function() {
   $(document).on('keydown','#prompt',function(e){
     clean_llm_ui('status');
     if (e.key === "Enter" && (e.metaKey || e.ctrlKey)) {
-      call_llm_service('status',this.id);
+      call_llm_service('status',this.id,'llms');
     }
   });
   $(document).on('click','#prompt_submit_button',function(e){
     clean_llm_ui('status');
-    call_llm_service('status','prompt');
+    call_llm_service('status','prompt','llms');
+  });
+  $(document).on('click','#generate_itext_button',function(e){
+    clean_llm_ui('status');
+    //console.log(data);
+    call_llm_text_service('status','prompt','llms','file');
+  });
+  $(document).on('click','#generate_text_button',function(e){
+    clean_llm_ui('status');
+    //console.log(data);
+    call_llm_text_service('status','prompt','llms','show');
+  });
+  $(document).on('click','#prompt_undo_button',function(e){
+    clean_llm_ui('status');
+    load_last_model_before_generation();
+  });
+  $(document).on('click','#prompt_attach_button',function(e){
+    document.getElementById('loadtxt').click();
+  });
+  $("#loadtxt").change(function(e){
+    let files = document.getElementById('loadtxt').files;
+    load_file_content(files);
+  });
+  $(document).on('drop','#prompt',function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    load_file_content(e.originalEvent.dataTransfer.files);
   });
 });
