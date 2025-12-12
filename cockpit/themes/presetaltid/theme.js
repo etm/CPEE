89f3ea77c49c45587affb1885b551b57a43c8af4
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2025-12-12 10:10:47 +0100

    presetaltid

--- cockpit/themes/presetaltid/theme.js
+++ cockpit/themes/presetaltid/theme.js
@@ -6,11 +6,35 @@ WFAdaptorManifestation = class extends WFAdaptorManifestationBase {
     this.elements.call.illustrator.label = function(node) { //{{{
       return [ { column: 'Label', value: $('> label',$(node).children('parameters')).text().replace(/^['"]/,'').replace(/['"]$/,'') + ' (' + $(node).attr('a:alt_id') + ')' } ];
     }; //}}}
+    this.elements.call.illustrator.info = function(node) { //{{{
+      return { 'element-endpoint': $(node).attr('endpoint'), 'element-alt_id': $(node).attr('a:alt_id')  };
+    }; //}}}
+    this.elements.callmanipulate.illustrator.label = function(node) { //{{{
+      return [ { column: 'Label', value: $('> label',$(node).children('parameters')).text().replace(/^['"]/,'').replace(/['"]$/,'') + ' (' + $(node).attr('a:alt_id') + ')' } ];
+    }; //}}}
+    this.elements.callmanipulate.illustrator.info = function(node) { //{{{
+      return { 'element-endpoint': $(node).attr('endpoint'), 'element-alt_id': $(node).attr('a:alt_id')  };
+    }; //}}}
     this.elements.stop.illustrator.label = function(node) { //{{{
       return [ { column: 'Label', value: $('> label',$(node).children('parameters')).text().replace(/^['"]/,'').replace(/['"]$/,'') + ' (' + $(node).attr('a:alt_id') + ')' } ];
     }; //}}}
+    this.elements.stop.illustrator.info = function(node) { //{{{
+      return { 'element-alt_id': $(node).attr('a:alt_id')  };
+    }; //}}}
     this.elements.wait_for_signal.illustrator.label = function(node) { //{{{
       return [ { column: 'Label', value: $('> label',$(node).children('parameters')).text().replace(/^['"]/,'').replace(/['"]$/,'') + ' (' + $(node).attr('a:alt_id') + ')' } ];
+    }; //}}}
+    this.elements.wait_for_signal.illustrator.info = function(node) { //{{{
+      return { 'element-alt_id': $(node).attr('a:alt_id')  };
+    }; //}}}
+    this.elements.choose.illustrator.info = function(node) { //{{{
+      return { 'element-alt_id': $(node).attr('a:alt_id')  };
+    }; //}}}
+    this.elements.loop.illustrator.info = function(node) { //{{{
+      return { 'element-alt_id': $(node).attr('a:alt_id')  };
+    }; //}}}
+    this.elements.parallel.illustrator.info = function(node) { //{{{
+      return { 'element-alt_id': $(node).attr('a:alt_id')  };
     }; //}}}
   }
 }
