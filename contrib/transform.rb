0106535a7ba1e75d8ee0a5225e339cf14f4bb188
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2025-12-14 02:18:45 +0100

    transform for manipulate code

---
+++ contrib/transform.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+require 'xml/smart'
+
+Dir.glob('**/*.xml').each do |f|
+  if File.file? f
+    XML::Smart.open(f) do |d|
+      if d.register_namespace 'd', 'http://cpee.org/ns/description/1.0'
+        d.find('//d:manipulate').each do |e|
+          if e.find('d:code').length == 0
+            t = e.text
+            e.text = ''
+            e.add('d:code',t)
+            puts e.dump
+          end
+        end
+      end
+    end
+  end
+end
