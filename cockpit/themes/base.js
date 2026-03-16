be3c1e644107ff5bdc2f1f941bf11a1daf3e2475
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2026-03-16 22:29:02 +0100

    manipulate fix

--- cockpit/themes/base.js
+++ cockpit/themes/base.js
@@ -1350,8 +1350,8 @@ function WFAdaptorManifestationBase(adaptor) {
             $('call > code > prepare',n).each(function(j,m){
               item += m.textContent + '\n';
             });
-            if (n.nodeName == 'manipulate') { // css selector can not directly access manipulate
-              item += n.textContent + '\n';
+            $('manipulate > code ',n).each(function(j,m){
+              item += m.textContent + '\n';
             }
             $('call > parameters > arguments *',n).each(function(j,m){
               let x = m.textContent;
