827a3a33180f88eafc9d30c61183ad4c1481cd55
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2026-04-28 22:33:37 +0200

    fix array assignment

--- cockpit/themes/dataflow/theme.js
+++ cockpit/themes/dataflow/theme.js
@@ -5,7 +5,7 @@ WFAdaptorManifestation = class extends WFAdaptorManifestationBase {
     this.striped = true;
     var dataflowExtract = function(subject,mixed,extract) { //{{{
       let dict = {};
-      var regassi =      /data\.([a-z_][a-zA-Z0-9_]*)\s*(=[^=]|\+\=|\-\=|\*\=|\/\=|<<|>>|\|\|=)/g; // we do not have to check for &gt;/&lt; version of stuff as only conditions are in attributes, and conditions can not contain assignments
+      var regassi =      /data\.([a-z_][a-zA-Z0-9_]*)[a-zA-Z0-9_\]\["']*\s*(=[^=]|\+\=|\-\=|\*\=|\/\=|<<|>>|\|\|=)/g; // we do not have to check for &gt;/&lt; version of stuff as only conditions are in attributes, and conditions can not contain assignments
       var reg_not_assi = /data\.([a-z_][a-zA-Z0-9_]*)\s*/g;
 
       $(subject).each(function(_,ele){
