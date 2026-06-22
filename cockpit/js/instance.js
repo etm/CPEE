e96f0a0a78e38acceeb2ae13a1d183b99838e659
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2026-06-22 11:17:13 +0200

    xml log access from viewer service

--- cockpit/js/instance.js
+++ cockpit/js/instance.js
@@ -586,13 +586,18 @@ function monitor_instance_values(type,vals) {// {{{
             var uuid = $(" > attributes > uuid",res).text();
             $("#current-log").show();
             $("#shifted-log").show();
+            $("#xml-log").show();
             $("#current-log").attr('href',$('body').attr('current-logs') + uuid + '.xes.yaml');
+            $("#xml-log").attr('href',$('body').attr('current-logs') + uuid + '.xes.xml');
             $("#shifted-log").attr('href',$('body').attr('current-logs') + uuid + '.xes.shift.yaml');
             if ($("#current-log").text() == '') {
               $("#current-log").text(uuid + '.xes.yaml');
             }
             if ($("#shifted-log").text() == '') {
               $("#shifted-log").text(uuid + '.xes.shift.yaml');
+            }
+            if ($("#xml-log").text() == '') {
+              $("#shifted-log").text(uuid + '.xes.xml');
             }
           }
         }
